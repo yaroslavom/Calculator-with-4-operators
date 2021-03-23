b470b2be0b27695f3de72be40ec97252b3f52d80
Yaroslavom <yarykomm@gmail.com>
2021-03-23 13:45:06 +0200

    getElementsByClassName

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-var buttonPlus = document.getElementById("buttonPlus");
-var buttonMinus = document.getElementById("buttonMinus");
-var buttonMultiply = document.getElementById("buttonMultiply");
-var buttonDevide = document.getElementById("buttonDevide");
+var operationButtons = document.getElementsByClassName("operation-button");
 
 var input1 = document.getElementById('number1') 
 var input2 = document.getElementById('number2') 
@@ -29,8 +26,6 @@ function onOperationButtonClick(eventObject) {
     var operation = clickedElement.innerHTML;
     makeOperation(operation);
 }
-
-var operationButtons = [buttonPlus, buttonMinus, buttonMultiply, buttonDevide];
 
 for (i = 0; i < operationButtons.length; i++) { 
     operationButtons[i].addEventListener('click', onOperationButtonClick);
